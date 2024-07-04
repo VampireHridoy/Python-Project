67cd5cf45f10af915b5edd8e4b67039be3e82052
Km Rokonuzzaman <105871564+VampireHridoy@users.noreply.github.com>
2024-07-04 22:54:04 +0600

    Add files via upload

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class human:
+    def __init(ma,name):
+        ma.name=name
+        print(ma.name +"Looking nice")
+        def run(ma):
+          print("running")
+class rana(human):
+   def run(ma):
+      print(" Dashing")
+      spot=rana("spot")
+      spot.run()
